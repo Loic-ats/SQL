195f7a635a19b674e66e5e34912dfb794e22ad73
Loïc ARTUSO <artusol@hotmail.fr>
2020-07-20 16:10:38 +0200

    TP 2 SQL

---
+++ bdd_test.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 20 juil. 2020 à 14:32
+-- Version du serveur :  10.4.13-MariaDB
+-- Version de PHP : 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `bdd_test`
+--
+CREATE DATABASE IF NOT EXISTS `bdd_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `bdd_test`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_clients`
+--
+
+DROP TABLE IF EXISTS `t_clients`;
+CREATE TABLE IF NOT EXISTS `t_clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(55) NOT NULL,
+  `prenom` varchar(55) NOT NULL,
+  `pseudo` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
